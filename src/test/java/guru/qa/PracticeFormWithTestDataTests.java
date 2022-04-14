8ad1884db56ca6b436046d79f1c1a9e119d91561
Ekaterina Tsyganova <orangenochew@gmail.com>
2022-04-14 13:59:45 +0300

    added tests with test data

--- src/test/java/guru/qa/PracticeFormWithTestDataTests.java
+++ src/test/java/guru/qa/PracticeFormWithTestDataTests.java
@@ -8,16 +8,19 @@ import org.openqa.selenium.Keys;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
+import static java.lang.String.format;
 
 
-public class PracticeFormTests {
+public class PracticeFormWithTestDataTests {
+    String firstName = "Taras",
+            lastName = "Bulba",
+            email = "taras@bulba.ua";
+    String expectedFullName = format("%s %s",firstName,lastName);
 
     @BeforeAll
     static void setUp(){
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.holdBrowserOpen = true;
-
-
     }
 
     @Test
@@ -27,44 +30,33 @@ public class PracticeFormTests {
         executeJavaScript("$('footer').remove()"); //убираем футер шоб кнопка влезла
         executeJavaScript("$('fixedban').remove()");
 
-        $("#firstName").setValue("Taras");
-        $("#lastName").setValue("Bulba");
-        $("#userEmail").setValue("taras@bulba.ua");
+        $("#firstName").setValue(firstName);
+        $("#lastName").setValue(lastName);
+        $("#userEmail").setValue(email);
         $("#gender-radio-1").doubleClick();
         $("#userNumber").setValue("8965462736");
-
-        // здесь бы вынести селектор в переменную, but how
         $("#dateOfBirthInput").click();
         $("#dateOfBirthInput").sendKeys((Keys.CONTROL + "a")); //выделяем
         $("#dateOfBirthInput").sendKeys(Keys.SPACE);           // и затираем поле
         $("#dateOfBirthInput").setValue("10 May 1938").pressEnter();
-
-        $("#subjectsInput").setValue("M").pressEnter();;
+        $("#subjectsInput").setValue("M").pressEnter();
         $(byText("Music")).click();
-
-        // картиночка
         $("#uploadPicture").uploadFromClasspath("Mikhail_Nesterov_001.jpg");
-
-        //address
         $("#currentAddress").setValue("Velliangiri Foothills, Ishana Vihar Post," +
                 "Coimbatore - 641 114");
-
         $("#state").click();
         $(byText("Rajasthan")).click(); //поиск по тексту так себе решение
-
         $("#city").click();
         $(byText("Jaipur")).click();
-
         $("#submit").click();
 
         //проверка результата
-        $(".table-responsive").shouldHave(text("Student Name Taras Bulba"),
-                text("Student Email taras@bulba.ua"),text("Gender Male"),
+        $(".table-responsive").shouldHave(text(expectedFullName),
+                text(email),text("Gender Male"),
                 text("Mobile 8965462736"),text("Date of Birth 10 May,1938"),
                 text("Subjects Maths"),text("Hobbies Music"),
                 text("Picture Mikhail_Nesterov_001.jpg"),
                 text("Address Velliangiri Foothills, Ishana Vihar Post,Coimbatore - 641 114"),
                 text("State and City Rajasthan Jaipur"));
     }
-
 }
